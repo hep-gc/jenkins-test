cefe148b03e3723033a1849a1ea085b1860d5a17
Mike Chester <m.chester@hotmail.com>
2013-01-15 10:47:50 -0800

    catches more errors and more generic then before

--- bootvm.py
+++ bootvm.py
@@ -3,39 +3,83 @@ import subprocess
 import re
 import sys
 import time
+import os
 
-if 'mchester' not in subprocess.Popen("/usr/local/bin/repoman whoami", shell=True, stdout=subprocess.PIPE).stdout.read():
-	print "myproxy-logon expired. Quitting..."
-	sys.exit()
+#Check myproxy credentials. If error, exit, else credentials are valid.
+process = subprocess.Popen(["/usr/local/bin/repoman","whoami"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process.wait()
+retcode = process.returncode
+out,err = process.communicate()
+if retcode != 0:
+    sys.exit(retcode)
 
-print "Booting virtual machine..."
-process = subprocess.Popen("/usr/local/bin/vm-run -s /var/lib/jenkins/.ssh/id_rsa.pub -i http://repoman.heprc.uvic.ca/api/images/raw/mchester/__hypervisor__/jenkins.gz",
-			    shell=True, 
-			    stdout=subprocess.PIPE, 
-			    stderr=subprocess.PIPE)
+#Boot VM if valid proxyuser.
+process = subprocess.Popen(["/usr/local/bin/vm-run","-s","/var/lib/jenkins/.ssh/id_rsa.pub","-R","jenkins.gz"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process.wait()
+out,err = process.communicate()
+print out
+if "Error:" in out:
+    sys.exit(retcode)
 
-for line in iter(process.stdout.readline, ""):
-	print line
-	if "Error:" in line:
-		sys.exit()
-	if "Hostname = " in line:
-		hostname = re.findall(r'Hostname = (.*?)\n',line)[0]
-
-if hostname is not '':
-	print "Virtual machine booting with hostname: " + hostname 
-	while '0 received' in subprocess.Popen("/bin/ping -c 1 -W 10 "+hostname, shell=True, stdout=subprocess.PIPE).stdout.read():
-		time.sleep( 5 )
-	print hostname + " is ready! You can now SSH into this virtual machine."
+#If VM is launched, recover hostname from output, and ping the machine until its ready.
+hostname = re.findall(r'Hostname = (.*?)\n', out)
+if not hostname:
+    print "Hostname could not be resolved."
+    sys.exit(1)
 else:
-	print "Hostname could not be resolved."
+    hostname = hostname[0]
+    print "Virtual machine booting with hostname " + hostname + "... Please wait."
+    process = subprocess.Popen(["ping","-c","1", hostname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process.wait()
+    retcode =  process.returncode
+    while retcode != 0:
+        process = subprocess.Popen(["ping","-c","1", hostname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.wait()
+        retcode =  process.returncode
+        time.sleep( 5 )
+    print "Virtual machine ready!"
+
+#Wait 10 seconds for SSH agent to become ready
+#Then copy echo.py script onto newly created VM and run it.
+print "Waiting for SSH Agent to become available..."
+
+filepath = "/var/lib/jenkins/workspace/bootvm/echo.py"
+filename = os.path.basename(filepath)
+vmpath = "/var/lib/jenkins/"
 
 time.sleep( 10 )
-print "Testing ssh"
 
-cmd = "/usr/bin/scp /var/lib/jenkins/workspace/bootvm/echo.py root@" + hostname + ":/var/lib/jenkins/"
-response =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()
-print response
+#copy file
+print "Copying " + filename + " into " + vmpath + " on " + hostname
 
-cmd = "/usr/bin/ssh root@" + hostname + " /var/lib/jenkins/echo.py"
-response =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()
-print response
+cmd = "/usr/bin/scp " + filepath + " root@" + hostname + ":" + vmpath
+process =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process.wait()
+retcode = process.returncode
+out,err = process.communicate()
+print out
+if retcode != 0:
+    print err
+    sys.exit(retcode)
+
+#run file
+cmd = "/usr/bin/ssh root@" + hostname + " " + vmpath + filename
+process =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process.wait()
+retcode = process.returncode
+out,err = process.communicate()
+print out
+if retcode != 0:
+    print err
+    sys.exit(retcode)
+
+#Check whether the hostname from vm-run and hostname returned from echo.py on the vm is the same.
+vmhostname = re.findall(r'Hostname: (.*?)\n', out)
+if vmhostname:
+    vmhostname = vmhostname[0]
+    if vmhostname == hostname:
+        print "Both machine hostnames match!"
+        sys.exit()
+    else:
+        print "Hostnames do not match."
+        sys.exit(1)
