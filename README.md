afa66974ad9a6161466c86c9dc2c9e7b5ba13cca
Mike Chester <m.chester@hotmail.com>
2013-01-17 13:38:41 -0800

    Added error catching for vm-run. Updated and added a few comments.
    Updated vm-run references to point to proper repo

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 #Overview
-Python script used to boot a virtual machine using **[repoman](https://github.com/hep-gc/repoman)** / **[vm-run](https://github.com/hep-gc/vm-helpers)**, it will then copy a user defined script into a user defined directory on the created virtual machine and run it, which can then be monitored by **[jenkins](http://jenkins-ci.org/)**.
+Python script used to boot a virtual machine using **[repoman](https://github.com/hep-gc/repoman)** / **[vm-run](https://github.com/hep-gc/production-helpers/blob/master/bin/vm-run)**, it will then copy a user defined script into a user defined directory on the created virtual machine and run it, which can then be monitored by **[jenkins](http://jenkins-ci.org/)**.
 
 Written for Python 2.7+.
 
 ##Usage
-Running this script acts in a similar fashion as **[vm-run](https://github.com/hep-gc/vm-helpers)**.
-It also features two additional parametres for copying a local file onto the newly booted virtual machine:
+Running this script acts in a similar fashion as **[vm-run](https://github.com/hep-gc/production-helpers/blob/master/bin/vm-run)**.
+It also features two additional parametres for copying a local file onto the newly booted virtual machine and running it:
 ```
 [-lp | --localpath]  - local path to file you want to copy onto created virtual machine.
 [-rp | --remotepath] - path on virtual machine where the file (see -lp) will be copied to.
--- bootvm.py
+++ bootvm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # Author: Mike Chester <mchester@uvic.ca>
 # Copyright (C) 2013 University of Victoria
@@ -35,11 +35,12 @@ def check_myproxy_logon():
         retcode = process.returncode
         out,err = process.communicate()
 
+        # if repoman does not return 0 an error has occured.
         if retcode != 0:
             # we don't have valid credential
             # repoman will tell you what the problem is 
             print out
-            sys.exit(1)
+            sys.exit(retcode)
     
     # if repoman is not found exception will be caught here.
     except OSError as e:
@@ -78,7 +79,15 @@ def boot_virtual_machine(cmd):
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process.wait()
 
+        retcode = process.returncode
         out,err = process.communicate()
+
+        # if vm-run does not return 0 an error has occured.
+        if retcode != 0:
+            # vm-run will tell you the error
+            print out
+            sys.exit(retcode)
+
         hostname = re.findall(r'Hostname = (.*?)\n', out)
         
         print out
@@ -133,7 +142,7 @@ def secure_copy_file(hostname,localpath,remotepath,filename):
         # if scp does not return 0, an error occured.
         if retcode != 0:
             print err
-            sys.exit(1)
+            sys.exit(retcode)
 
     # if scp is not found exception will be caught here. 
     except OSError as e:
@@ -160,7 +169,7 @@ def run_remote_file(hostname,remotepath,filename):
         # if ssh return code is not 0, an error occured. 
         if retcode != 0:
             print err
-            sys.exit(1)
+            sys.exit(retcode)
         
         # test the output of the ssh command with the hostname recovered from the vm-run command.
         sanity_check(hostname,out)
